e670d61d9a28f8940d8e5b8b2b98494892f3663e
FidelisSantos <vitorfidelisantos@gmail.com>
2022-10-19 12:44:52 -0300

    Arrumando dotenv

--- src/firebase/Connection.ts
+++ src/firebase/Connection.ts
@@ -1,18 +1,19 @@
 import { initializeApp } from "firebase/app";
-import  firebase ,{collection, getFirestore} from "firebase/firestore"
+import  {collection, getFirestore} from "firebase/firestore"
+import * as dotenv from 'dotenv';
 
+dotenv.config()
 const firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  projectId: ,
-  storageBucket: ,
-  messagingSenderId: ,
-  appId: ,
-  measurementId: 
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASURENMENTID
 };
-
 // Initialize Firebase
-const appFirebase = initializeApp(firebaseConfig);
+const appFirebase = initializeApp(firebaseConfig)
 
 export const firestore = getFirestore(appFirebase);
 
